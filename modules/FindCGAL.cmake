eaf41ef336c4e72ef8e239b2c69d252870ce1c27
Mateusz Loskot <mateusz@loskot.net>
2010-12-24 00:17:29 +0000

    Added FindCGAL.cmake. Stolen from http://github.com/pgRouting

---
+++ modules/FindCGAL.cmake
@@ -0,0 +1,44 @@
+# - Find CGAL
+# Source: https://github.com/pgRouting/
+# Find the CGAL includes and client library
+# This module defines
+#  CGAL_INCLUDE_DIR, where to find CGAL.h
+#  CGAL_LIBRARIES, the libraries needed to use CGAL.
+#  CGAL_FOUND, If false, do not try to use CGAL.
+
+if(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
+   set(CGAL_FOUND TRUE)
+
+else(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
+
+ FIND_PATH(CGAL_INCLUDE_DIR CGAL/basic.h
+      /usr/include
+      /usr/local/include
+      $ENV{ProgramFiles}/CGAL/*/include
+      $ENV{SystemDrive}/CGAL/*/include
+      )
+
+  find_library(CGAL_LIBRARIES NAMES CGAL libCGAL
+     PATHS
+     /usr/lib
+     /usr/local/lib
+     /usr/lib/CGAL
+     /usr/lib64
+     /usr/local/lib64
+     /usr/lib64/CGAL
+     $ENV{ProgramFiles}/CGAL/*/lib
+     $ENV{SystemDrive}/CGAL/*/lib
+     )
+      
+  if(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
+    set(CGAL_FOUND TRUE)
+    message(STATUS "Found CGAL: ${CGAL_INCLUDE_DIR}, ${CGAL_LIBRARIES}")
+    INCLUDE_DIRECTORIES(${CGAL_INCLUDE_DIR} $ENV{CGAL_CFG})
+  else(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
+    set(CGAL_FOUND FALSE)
+    message(STATUS "CGAL not found.")
+  endif(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
+
+  mark_as_advanced(CGAL_INCLUDE_DIR CGAL_LIBRARIES)
+
+endif(CGAL_INCLUDE_DIR AND CGAL_LIBRARIES)
