1ed01c0e11ed37a06ddf892d0d4181055ffab659
Mateusz Loskot <mateusz@loskot.net>
2018-05-12 00:26:26 +0200

    Add FindODBC.cmake status

--- modules/registry.cmake
+++
@@ -1,6 +0,0 @@
-get_filename_component(_windows10_kits_root_dir
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]"
-  ABSOLUTE)
-message("X ${_windows10_kits_root_dir}")
-file(GLOB _windows10_kits_dirs LIST_DIRECTORIES TRUE "${_windows10_kits_root_dir}")
-message("Z ${_windows10_kits_dirs}")
