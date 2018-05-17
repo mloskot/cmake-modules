3263e7dd45109e947a36bd87a9bb40fadc42446b
Mateusz Loskot <mateusz@loskot.net>
2018-05-17 17:51:23 +0100

    FindODBC: do not look for odbc32 in SDKs on Windows

--- modules/FindODBC.cmake
+++ modules/FindODBC.cmake
@@ -1,101 +1,240 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
-#.rst:
-# FindODBC
-# --------
-#
-# Find the ODBC library
-#
+#[=======================================================================[.rst:
+FindODBC
+--------
 
-### Windows SDK ###############################################################
-if(WIN32)
-  # Guess what architecture targets will end up being built
-  if("${CMAKE_GENERATOR}" MATCHES "(Win64|IA64)")
-    set(arch_hint "x64")
-  elseif("${CMAKE_GENERATOR_PLATFORM}" MATCHES "ARM64")
-    set(arch_hint "arm64")
-  elseif("${CMAKE_GENERATOR}" MATCHES "ARM")
-    set(arch_hint "arm")
-  elseif("$ENV{LIB}" MATCHES "(amd64|ia64)")
-    set(arch_hint "x64")
-  elseif("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
-    set(arch_hint "x64")
-  endif()
+Find the ODBC include directory and library.
 
-  if(NOT arch_hint)
-    set(arch_hint "x86")
-  endif()
+Use this module by invoking find_package with the form::
 
-  # Find the Windows 10 SDKs directories.
-  get_filename_component(_windows10_kits_root_dir
-    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]"
-    ABSOLUTE)
-  file(GLOB _windows10_kits_dirs LIST_DIRECTORIES TRUE
-    "${_windows10_kits_root_dir}/include/*/um")
-  unset(_windows10_kits_root_dir)
-  # Poor-man ordering of 10.0.NNNNNN.0 folders from newest to oldest
-  list(SORT _windows10_kits_dirs)
-  list(REVERSE _windows10_kits_dirs)
-  list(APPEND _odbc_include_hints ${_windows10_kits_dirs})
-  unset(_windows10_kits_root_dir)
+.. code-block:: cmake
 
-  # Determine "c:\Program Files" location of SDKs with ODBC development files
-  if("$ENV{ProgramW6432}" STREQUAL "$ENV{ProgramFiles}")
-    set(_program_files "ProgramFiles(x86)")
-  else()
-    set(_program_files "ProgramFiles")
-  endif()
-  file(TO_CMAKE_PATH "$ENV{${_program_files}}" _program_files)
-  # Ordered to search kits from newest to oldest
-  set(_odbc_include_paths
-    "${_program_files}/Windows Kits/10/include/10.0.17134.0/um"
-    "${_program_files}/Windows Kits/10/include/10.0.16299.0/um"
-    "${_program_files}/Windows Kits/10/include/*/um"
-    "${_program_files}/Windows Kits/8.1/include/um"
-    "${_program_files}/Windows Kits/8.0/include/um"
-    "${_program_files}/Windows Kits/*/include/um"
-    "${_program_files}/Microsoft SDKs/Windows/v7.1A/include"
-    "${_program_files}/Microsoft SDKs/Windows/v7.0A/include"
-    "${_program_files}/Microsoft SDKs/Windows/v6.0A/include"
-    "${_program_files}/Microsoft SDKs/Windows/*/include"
+  find_package(ODBC
+    [REQUIRED]             # Fail with error if ODBC is not found
   )
-  unset(_program_files)
+
+On Windows, when building with Visual Studio, this module assumes the ODBC
+library is provided by the available Windows SDK.
+
+On Unix, this module allows to search for ODBC library provided by
+unixODBC or iODBC implementations of ODBC API.
+This module reads hint about location of the config program:
+
+.. variable:: ODBC_CONFIG
+
+  Location of odbc_config or iodbc-config program
+
+Otherwise, this module tries to find the config program,
+first from unixODBC, then from iODBC.
+If no config program found, this module searches for ODBC header
+and library in list of known locations.
+
+Imported targets
+^^^^^^^^^^^^^^^^
+
+This module defines the following :prop_tgt:`IMPORTED` targets:
+
+.. variable:: ODBC::ODBC
+
+  Imported target for using the ODBC library, if found.
+
+Result variables
+^^^^^^^^^^^^^^^^
+
+.. variable:: ODBC_FOUND
+
+  Set to true if ODBC library found, otherwise false or undefined.
+
+.. variable:: ODBC_INCLUDE_DIRS
+
+  Paths to include directories listed in one variable for use by ODBC client.
+
+.. variable:: ODBC_LIBRARIES
+
+  Paths to libraries to linked against to use ODBC.
+
+.. variable:: ODBC_CONFIG
+
+  Path to unixODBC or iODBC config program, if found or specified.
+
+Cache variables
+^^^^^^^^^^^^^^^
+
+For users who wish to edit and control the module behavior, this module
+reads hints about search locations from the following variables::
+
+.. variable:: ODBC_INCLUDE_DIR
+
+  Path to ODBC include directory with ``sql.h`` header.
+
+.. variable:: ODBC_LIBRARY
+
+  Path to ODBC library to be linked.
+
+NOTE: The variables above should not usually be used in CMakeLists.txt files!
+
+Limitations
+^^^^^^^^^^^
+
+On Windows, this module does not search for iODBC.
+On Unix, there is no way to prefer unixODBC over iODBC, or vice versa,
+other than providing the config program location using the ``ODBC_CONFIG``.
+This module does not allow to search for a specific ODBC driver.
+
+#]=======================================================================]
+
+### Try Windows Kits ##########################################################
+if(WIN32)
+  # List names of ODBC libraries on Windows
+  set(ODBC_LIBRARY odbc32)
+  set(_odbc_lib_names ${ODBC_LIBRARY};)
+
+  # List additional libraries required to use ODBC library
+  if(MSVC OR CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+    set(_odbc_required_libs_names odbccp32;ws2_32)
+  elseif(MINGW)
+    set(_odbc_required_libs_names odbccp32)
+  endif()
 endif()
 
-### ODBC_INCLUDE_DIR ##########################################################
+### Try nixODBC or iODBC config program #######################################
+if (UNIX AND NOT ODBC_CONFIG)
+  find_program(ODBC_CONFIG
+    NAMES odbc_config iodbc-config
+    DOC "Path to unixODBC or iODBC config program")
+endif()
+
+if (UNIX AND ODBC_CONFIG)
+  # unixODBC and iODBC accept unified command line options
+  execute_process(COMMAND ${ODBC_CONFIG} --cflags
+    OUTPUT_VARIABLE _cflags OUTPUT_STRIP_TRAILING_WHITESPACE)
+  execute_process(COMMAND ${ODBC_CONFIG} --libs
+    OUTPUT_VARIABLE _libs OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+  # Collect paths of include directories from CFLAGS
+  string(REGEX MATCHALL "\-I(/[^/ ]+)+" _cflags ${_cflags})
+  foreach(_path IN LISTS _cflags)
+    string(REGEX REPLACE "\-I(/[^/ ]+)" "\\1" _path ${_path})
+    list(APPEND _odbc_include_paths ${_path})
+  endforeach()
+  unset(_cflags)
+
+  # Collect paths of lib directories from LIBS
+  string(REGEX MATCHALL "\-L(/[^/ ]+)+" _lib_paths "${_libs}")
+  foreach(_path IN LISTS _lib_paths)
+    string(REGEX REPLACE "\-L(/[^/ ]+)" "\\1" _path ${_path})
+    list(APPEND _odbc_lib_paths ${_path})
+    string(REPLACE "-L${_path}" "" _libs ${_libs})
+  endforeach()
+
+  # Collect names of libraries from LIBS
+  string(REGEX MATCHALL "\-l[a-zA-Z0-9_-]+" _libs ${_libs})
+  set(_z -ldl;-lpthread;)
+  foreach(_lib IN LISTS _libs _z)
+    string(REGEX REPLACE "-l([a-zA-Z0-9_-]+)" "\\1" _lib ${_lib})
+    string(REGEX MATCH "odbc" _is_odbc ${_lib})
+    if(_is_odbc)
+      list(APPEND _odbc_lib_names ${_lib})
+    else()
+      list(APPEND _odbc_required_libs_names ${_lib})
+    endif()
+  endforeach()
+  unset(_libs)
+endif()
+
+### Try unixODBC or iODBC in include/lib filesystems ##########################
+if (UNIX AND NOT ODBC_CONFIG)
+  # List names of both ODBC libraries, unixODBC and iODBC
+  set(_odbc_lib_names odbc;iodbc;unixodbc;)
+
+  set(_odbc_include_paths
+    /usr/include
+    /usr/include/odbc
+    /usr/local/include
+    /usr/local/include/odbc
+    /usr/local/odbc/include)
+
+  set(_odbc_lib_paths
+    /usr/lib
+    /usr/lib/odbc
+    /usr/local/lib
+    /usr/local/lib/odbc
+    /usr/local/odbc/lib)
+endif()
+
+# DEBUG
+#message("_odbc_include_hints=${_odbc_include_hints}")
+#message("_odbc_include_paths=${_odbc_include_paths}")
+#message("_odbc_lib_paths=${_odbc_lib_paths}")
+#message("_odbc_lib_names=${_odbc_lib_names}")
+
+### Find include directories ##################################################
 find_path(ODBC_INCLUDE_DIR
   NAMES sql.h
   HINTS ${_odbc_include_hints}
-  PATHS ${_odbc_include_paths}
-)
-unset(_odbc_include_hints)
-unset(_odbc_include_paths)
+  PATHS ${_odbc_include_paths})
 
-### ODBC_LIBRARIES ############################################################
-if(WIN32 AND ODBC_INCLUDE_DIR)
-  string(REGEX REPLACE "[Ii]nclude" "lib" _odbc_lib_paths ${ODBC_INCLUDE_DIR})
-  string(APPEND _odbc_lib_paths "/${arch_hint}")
-  file(TO_CMAKE_PATH "${_odbc_lib_paths}" _odbc_lib_paths)
+if(NOT ODBC_INCLUDE_DIR AND WIN32)
+  set(ODBC_INCLUDE_DIR "")
 endif()
 
-find_library(_odbc32_lib_path
-  NAMES odbc32
-  PATHS ${_odbc_lib_paths}
-)
-unset(_odbc_lib_paths)
+### Find libraries ############################################################
+if(NOT ODBC_LIBRARY)
+  find_library(ODBC_LIBRARY
+    NAMES ${_odbc_lib_names}
+    PATHS ${_odbc_lib_paths})
 
-if(_odbc32_lib_path)
-  set(ODBC_LIBRARIES ${_odbc32_lib_path})
+  foreach(_lib IN LISTS _odbc_required_libs_names)
+    find_library(_lib_path
+      NAMES ${_lib}
+      PATHS ${_odbc_lib_paths}) # system parths or collected from ODBC_CONFIG
+    if (_lib_path)
+      list(APPEND _odbc_required_libs_paths ${_lib_path})
+    endif()
+    unset(_lib_path CACHE)
+  endforeach()
+
+  unset(_odbc_lib_names)
+  unset(_odbc_lib_paths)
+  unset(_odbc_required_libs_names)
 endif()
-unset(_odbc32_lib_path)
+
+### Set result variables ######################################################
+set(REQUIRED_VARS ODBC_LIBRARY)
+if(NOT WIN32)
+  list(APPEND REQUIRED_VARS ODBC_INCLUDE_DIR)
+endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ODBC
-  DEFAULT_MSG ODBC_LIBRARIES ODBC_INCLUDE_DIR
-)
+find_package_handle_standard_args(ODBC DEFAULT_MSG ${REQUIRED_VARS})
 
-mark_as_advanced(ODBC_LIBRARIES ODBC_INCLUDE_DIR)
+mark_as_advanced(FORCE ODBC_LIBRARY ODBC_INCLUDE_DIR)
 
-### ODBC targets ##############################################################
-# TODO
+if(ODBC_CONFIG)
+  mark_as_advanced(FORCE ODBC_CONFIG)
+endif()
+
+set(ODBC_INCLUDE_DIRS ${ODBC_INCLUDE_DIR})
+list(APPEND ODBC_LIBRARIES ${ODBC_LIBRARY})
+list(APPEND ODBC_LIBRARIES ${_odbc_required_libs_paths})
+
+### Import targets ############################################################
+if(ODBC_FOUND)
+  if(NOT TARGET ODBC::ODBC)
+    add_library(ODBC::ODBC UNKNOWN IMPORTED)
+    set_target_properties(ODBC::ODBC
+      PROPERTIES
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+      IMPORTED_LOCATION "${ODBC_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${ODBC_INCLUDE_DIR}")
+
+    if(_odbc_required_libs_paths)
+      set_property(TARGET ODBC::ODBC APPEND PROPERTY
+        INTERFACE_LINK_LIBRARIES "${_odbc_required_libs_paths}")
+    endif()
+  endif()
+endif()
+
+unset(_odbc_required_libs_paths)
